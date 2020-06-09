2b8cafe685887d5433de62a44ab964ef754f0699
noxsios <harryrndzz@gmail.com>
2020-06-09 21:17:56 +0200

    Added Readme.md, almost done with problem

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # polymorphismChallenge
 Galvanize Polymorphism Challenge
+
+*//create a function that takes in an integer for a person's age and an array of objects of type : "country minimum age checker" that checks the minimum age for different activities in different countries*
+
+
+
+age = 17
+
+
+
+array = [CountryMinimumAgeChecker, UsaMinimumAgeChecker, CanadaMinimumAgeChecker, FranceMinimumAgeChecker]
+
+
+
+minimumAgeChecker(age, array)
+
+*//prints the below output to the screen (these responses may not be accurate but is just an example)*
+
+```
+*//you can drive in most countries:false*
+*//you can drive in most countries:false*
+*//you can drive in most countries:false*
+
+*//you can drive in United States: true*
+*//you can vote in United States: false*
+*//you can drink in United States: false*
+
+*//you can drive in Canada: true*
+*//you can vote in Canada: true*
+*//you can drink in Canada: false*
+
+*//you can drive in France: true*
+*//you can vote in France: true*
+*//you can drink in France: false*
+```
+
+* //driving -[Driving Link](https://en.wikipedia.org/wiki/List_of_minimum_driving_ages)
+	* // Canada - 16
+	* // France - 15
+	* // USA - 18
+
+- //voting - [Voting Link](https://en.wikipedia.org/wiki/Voting_age)
+  - // Canada - 18
+  - // France - 18
+  - // USA - 18
+
+* //drinking - [Drinking Link](https://en.wikipedia.org/wiki/Legal_drinking_age)
+  * // Canada - 18
+  * // France - 18
+  * // USA - 21
+
+```javascript
+//for extra credit create another function that takes an array of person objects like the one below and prints to the screen their status for an array of country checkers*
+
+person = { name: "John Doe", age: 17 }
+
+minimumAgeChecker(age, array)
+```
+
+
+
+```javascript
+*//for extra credit add additional countries other than Usa,Canada, and France to the array and create minimum age checker classes for them*
+```
+
+
+
+> [Github Repo Here](https://github.com/Noxsios/polymorphismChallenge)
---
+++ main.js
@@ -0,0 +1,76 @@
+class CountryMinimumAgeChecker {
+    //set the default age for driving,drinking,and voting to be 21
+    constructor(name) {
+        this.name = name
+    }
+    isOfDrivingAge(value) {
+        return (value >= 21)
+    }
+    isOfVotingAge(value) {
+        return (value >= 21)
+    }
+    isOfDrinkingAge(value) {
+        return (value >= 21)
+    }
+}
+class UsaMinimumAgeChecker extends CountryMinimumAgeChecker {
+    isOfDrivingAge(value) {
+        return (value >= 18)
+    }
+    isOfVotingAge(value) {
+        return (value >= 18)
+    }
+    isOfDrinkingAge(value) {
+        return (value >= 21)
+    }
+}
+class CanadaMinimumAgeChecker extends CountryMinimumAgeChecker {
+    isOfDrivingAge(value) {
+        return (value >= 16)
+    }
+    isOfVotingAge(value) {
+        return (value >= 18)
+    }
+    isOfDrinkingAge(value) {
+        return (value >= 18)
+    }
+}
+class FranceMinimumAgeChecker extends CountryMinimumAgeChecker {
+    isOfDrivingAge(value) {
+        return (value >= 15)
+    }
+    isOfVotingAge(value) {
+        return (value >= 17)
+    }
+    isOfDrinkingAge(value) {
+        return (value >= 18)
+    }
+}
+//create a function that takes in an integer for a person's age and an array of objects of type : "country minimum age checker" 
+//that checks the minimum age for different activities in different countries
+let USA = new UsaMinimumAgeChecker('USA')
+let France = new FranceMinimumAgeChecker('France')
+let Canada = new CanadaMinimumAgeChecker('Canada')
+let defaultCheck = new CountryMinimumAgeChecker('most countries')
+
+function minimumAgeChecker(age, array) {
+    array.forEach(element => {
+        console.log(`You can drive in ${element.name}:${element.isOfDrivingAge(age)}`)
+        console.log(`You can vote in ${element.name}:${element.isOfVotingAge(age)}`)
+        console.log(`You can drink in ${element.name}:${element.isOfDrinkingAge(age)}`)
+        console.log("\n")
+    });
+}
+
+age = 17
+array = [defaultCheck, USA, Canada, France]
+minimumAgeChecker(age, array)
+
+// //for extra credit create another function that takes an array of person objects like the one below and prints to the screen their status for an array of country checkers
+// person = { name: "John Doe", age: 17 }
+
+// minimumAgeChecker(age, array)
+
+//for extra credit add additional countries other than Usa,Canada, and France to the array and create minimum age checker classes for them
+
+
